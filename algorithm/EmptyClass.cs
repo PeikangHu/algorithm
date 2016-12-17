bd33f32fc8dc49805076e8072973717bae6d1f50
Peter Hu <peikang.hu@gmail.com>
2016-12-17 17:09:44 -0600

    + Added the project

---
+++ algorithm/EmptyClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace algorithm
+{
+	public class EmptyClass
+	{
+		public EmptyClass()
+		{
+			
+		}
+
+		public int run() 
+		{
+			return 0;
+		}
+	}
+}
---
+++ algorithm/EmptyClass.test.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Xunit;
+
+namespace algorithm
+{
+    public class EmptyClassTest
+	{
+		[Fact]
+		public void test1()
+		{
+			Assert.Equal(3, Math.Max(3, 2));
+		}
+	}
+}
